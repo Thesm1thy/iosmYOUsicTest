ad144d413ceac88b4eaee65492875461f7721a9a
Thesm1thy <jsmit30@unh.newhaven.edu>
2021-03-10 18:43:48 -0500

    Initial Commit

---
+++ mYOUsicTest1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mYOUsicTest1
+//
+//  Created by Jason Smith on 3/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mYOUsicTest1/mYOUsicTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  mYOUsicTest1App.swift
+//  mYOUsicTest1
+//
+//  Created by Jason Smith on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct mYOUsicTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
